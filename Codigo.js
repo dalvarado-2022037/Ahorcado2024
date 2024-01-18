75768c84e9437ea64a031b7e8ff0c66b90805663
dalvarado-2022037 <135673406+dalvarado-2022037@users.noreply.github.com>
2024-01-18 17:32:41 -0600

    Se a agregado la funcion de detectar que letra pulso desactivar el boton y determinar si esta en la palabra o no

--- Codigo.js
+++ Codigo.js
@@ -7,10 +7,12 @@ letrasPalabra = [];
 for(i=0;i<palabraSeleccionada.length;i++){
     letrasPalabra += palabraSeleccionada.charAt(i);
 }
-function Pulsar(variable){
+
+function Pulsar(variable, id){
+    boton = document.getElementById(id);
     for (i=0;i<palabraSeleccionada.length;i++){
-        document.write("\nNumero: "+i+" Letra: "+variable+" Busqueda "+letrasPalabra[i]);
-        if(variable==letrasPalabra[i]) document.write("Encontro la letra");
+        if(variable==letrasPalabra[i]) boton.style="background-color: red;";
+        boton.disabled="false";
         letrasPalabra[i];
     }
 }
